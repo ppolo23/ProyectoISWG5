22913f2bc629cfb2379c1a0fb72bcb01f57b8d9e
RobertoAlc <33253474+RobertoAlc@users.noreply.github.com>
2017-12-16 13:05:31 +0100

    Controladores cambiados

--- Espacios/espacios_controller.py
+++ Espacios/espacios_controller.py
@@ -10,220 +10,206 @@ from ..util import deserialize_date, deserialize_datetime
 
 def conectar():
 
-    conexion = psycopg2.connect(
-        database = "MyDB",
-        user = "postgres",
-        password = "postgres",
-        host = "127.0.0.1",
-        port = "5432"
-    )
+	conexion = psycopg2.connect(
+		database = "espacios",
+		user = "postgres",
+		password = "postgres",
+		host = "127.0.0.1",
+		port = "5432"
+	)
 
-    return conexion
+	return conexion
 
 def lanzarError(msg, status, title, typee):
-    """
-    Lanza un mensaje de \"error\" en forma de jSon
-    """
-    d = {}
-    d["detail"] = msg
-    d["error"] = status
-    d["title"] = title
-    d["type"] = typee
-    return d
+	"""
+	Lanza un mensaje de \"error\" en forma de jSon
+	"""
+	d = {}
+	d["detail"] = msg
+	d["error"] = status
+	d["title"] = title
+	d["type"] = typee
+	return d
 
 def alquilar_espacio_put(alquilado):
-    """
-    Alquilar un espacio
-    Alquila un �nico espacio mediante un registro json
-    :param alquilado: Espacio que se necesita alquilar
-    :type alquilado: dict | bytes
+	"""
+	Alquilar un espacio
+	Alquila un ?nico espacio mediante un registro json
+	:param alquilado: Espacio que se necesita alquilar
+	:type alquilado: dict | bytes
 
-    :rtype: None
-    """
-    if connexion.request.is_json:
-        alquilado = Alquilado.from_dict(connexion.request.get_json())
+	:rtype: None
+	"""
+	if connexion.request.is_json:
+		alquilado = Alquilado.from_dict(connexion.request.get_json())
+
+		print(alquilado)
 	
-	
-        conex = conectar()
-        cursor = conex.cursor()
-        
-        num = cursor.execute(
-            'SELECT COUNT(*) \
-             FROM alquilados ;')
-        
-        cursor.execute(
-            'INSERT INTO "alquilados" VALUES (espacio.cod_id,espacio.fecha,espacio.horaInicio,espacio.horaFin,espacio.id_prof)' + ";"
-        )
+		conex = conectar()
+		cursor = conex.cursor()
 
-        #rows = cursor.fetchall()  #lista de elementos que contiene la query
+		try:
+			cursor.execute(
+			"INSERT INTO alquilados VALUES (" + str(alquilado.cod_id) +
+											",\'" + str(alquilado.fecha) + "\'" +
+											",\'" + str(alquilado.hora_inicio) + "\'" +
+											",\'" + str(alquilado.hora_fin) + "\'" + 
+											"," + str(alquilado.id_prof) + ");")
+		except Exception as e:
+			return e
 
-        num2 = cursor.execute(
-            'SELECT COUNT(*) \
-             FROM alquilados ;')
-        
-        conex.close()
-    
-        if num2 == num:
-            return lanzarError("Problema alquilando, intentelo de nuevo", 404, "Error", "about:blank")
+		conex.commit()
+		conex.close()
 
-        else:
-            return lanzarError("Espacio Alquilado correctamente!!", 200, "Genial!", "about:blank")
-	
-	
-    return 'do some magic!'
-
+	return "Reserva correcta"
 
 def espacio_cod_id_get(codId):
-    """
-    Devuelve un espacio
-    Devuelve un �nico espacio identificado por su codId de espacio
-    :param codId: N�mero de identificaci�n del espacio
-    :type codId: str
+	"""
+	Devuelve un espacio
+	Devuelve un ?nico espacio identificado por su codId de espacio
+	:param codId: N?mero de identificaci?n del espacio
+	:type codId: str
+	:rtype: Espacio
+	"""
 
-    :rtype: Espacio
-    """
-    conex = conectar()                  #conectarse a la base de datos
-    cursor = conex.cursor()             #
+	conex = conectar()
+	cursor = conex.cursor()
 
-    cursor.execute(
-        "SELECT row_to_json(espacio) \
-         FROM espacios \
-         WHERE cod_id = " + str(codId) + ";"
-    )
+	cursor.execute(
+		"SELECT row_to_json(espacios) \
+		 FROM espacios \
+		 WHERE cod_id = " + str(codId) + ";"
+	)
 
-    rows = cursor.fetchall()
+	rows = cursor.fetchall()
 
-    conex.close()
+	conex.close()
 
-    if len(rows) == 0:
-        return lanzarError("Espacio no encontrado", 404, "Error", "about:blank")
+	if len(rows) == 0:
+		return lanzarError("Espacio no encontrado", 404, "Error", "about:blank")
 
-    else:
-        return rows[0][0]
+	else:
+		return rows[0][0]
 
 
 def espacio_facultad_get(facultad):
-    """
-    Devuelve un espacio
-    Devuelve un �nico espacio identificado por la facultad a la que pertenece
-    :param facultad: facultad del espacio
-    :type facultad: str
+	"""
+	Devuelve un espacio
+	Devuelve un ?nico espacio identificado por la facultad a la que pertenece
+	:param facultad: facultad del espacio
+	:type facultad: str
 
-    :rtype: Espacio
-    """
-    conex = conectar()
-    cursor = conex.cursor()
+	:rtype: Espacio
+	"""
+	conex = conectar()
+	cursor = conex.cursor()
 
-    cursor.execute(
-        "SELECT array_to_json(array_agg(row_to_json(espacio))) \
-         FROM espacios,alquilados,reservados \
-         WHERE espacios.facultad = " + str(facultad) + " AND \
-         espacios.cod_id IN (reservados OR alquilados);"
-    )
+	cursor.execute(
+		"SELECT array_to_json(array_agg(row_to_json(espacios))) \
+		 FROM \"espacios\" \
+		 WHERE espacios.facultad = '" + str(facultad) + "';"
+	)
 
-    rows = cursor.fetchall()  #lista de elementos que contiene la query
+	rows = cursor.fetchall()  #lista de elementos que contiene la query
 
-    conex.close()
+	conex.close()
 
-    if len(rows) == 0:
-        return lanzarError("Espacio no encontrado", 404, "Error", "about:blank")
 
-    else:
-        return rows[0][0]
+	if len(rows) == 0:
+		return lanzarError("Espacio no encontrado", 404, "Error", "about:blank")
+
+	else:
+		return rows[0][0]
 
 
 def get_espacios_ocupados():
-    """
-    Espacios de una facultad ocupados.
-    Devuelve los espacios ocupados de la facultad.
+	"""
+	Espacios de una facultad ocupados.
+	Devuelve los espacios ocupados de la facultad.
 
-    :rtype: List[Espacio]
-    """
-        conex = conectar()
-    cursor = conex.cursor()
+	:rtype: List[Espacio]
+	"""
+	conex = conectar()
+	cursor = conex.cursor()
 
-    cursor.execute(
-        "SELECT array_to_json(array_agg(row_to_json(espacio))) \
-         FROM espacios,alquilados,reservados \
-         WHERE espacios.cod_id IN (reservados OR alquilados);"
-    )
+	cursor.execute(
+	   "SELECT array_to_json(array_agg(row_to_json(t)))\
+		FROM (SELECT espacios.cod_id, espacios.nombre, espacios.facultad, tipo, capacidad, preciohora\
+		FROM espacios, reservados, alquilados\
+		WHERE espacios.cod_id = reservados.cod_id OR espacios.cod_id = alquilados.cod_id) as t;"
+		)
 
-    rows = cursor.fetchall()  #lista de elementos que contiene la query
+	rows = cursor.fetchall()  #lista de elementos que contiene la query
 
-    conex.close()
+	conex.close()
 
-    if len(rows) == 0:
-        return lanzarError("No hay espacios ocupados", 404, "Error", "about:blank")
+	if len(rows) == 0:
+		return lanzarError("No hay espacios ocupados", 404, "Error", "about:blank")
 
-    else:
-        return rows[0][0]
-#    return 'do some magic!'
+	else:
+		return rows[0][0]
 
 
 def obtener_espacio():
-    """
-    Obtiene espacios
-    Obtiene un listado de espacios del sistema.
+	"""
+	Obtiene espacios
+	Obtiene un listado de espacios del sistema.
 
-    :rtype: List[Espacio]
-    """
-    conex = conectar()
-    cursor = conex.cursor()
+	:rtype: List[Espacio]
+	"""
+	conex = conectar()
+	cursor = conex.cursor()
 
-    cursor.execute(
-        "SELECT array_to_json(array_agg(row_to_json(espacio))) \
-         FROM espacios ;"
-    )
+	cursor.execute(
+		"SELECT array_to_json(array_agg(row_to_json(espacios))) \
+		 FROM espacios ;"
+	)
 
-    rows = cursor.fetchall()  #lista de elementos que contiene la query
+	rows = cursor.fetchall()  #lista de elementos que contiene la query
 
-    conex.close()
+	conex.close()
 
-    if len(rows) == 0:
-        return lanzarError("No hay espacios", 404, "Error", "about:blank")
+	if len(rows) == 0:
+		return lanzarError("No hay espacios", 404, "Error", "about:blank")
 
-    else:
-        return rows[0][0]
-    
-    #return 'do some magic!'
+	else:
+		return rows[0][0]
+	
+	#return 'do some magic!'
 
 
 def reserva_espacio_put(reservado):
-    """
-    Reservar un espacio
-    Reserva un �nico espacio mediante un registro json
-    :param reservado: Espacio que se necesita reservar
-    :type reservado: dict | bytes
+	"""
+	Reservar un espacio
+	Reserva un ?nico espacio mediante un registro json
+	:param reservado: Espacio que se necesita reservar
+	:type reservado: dict | bytes
 
-    :rtype: None
-    """
-    if connexion.request.is_json:
-        reservado = Reservado.from_dict(connexion.request.get_json())
+	:rtype: None
+	"""
+	if connexion.request.is_json:
+		reservado = Reservado.from_dict(connexion.request.get_json())
 
-        conex = conectar()
-        cursor = conex.cursor()
-        
-        num = cursor.execute(
-            'SELECT COUNT(*) \
-             FROM reservados ;')
-        
-        cursor.execute(
-            'INSERT INTO "reservados" VALUES (espacio.cod_id,espacio.fechaInicio,espacio.diaSemana,espacio.fechaFin,espacio.horaInicio,espacio.horaFin,espacio.id_grupo)' + ";"
-        )
+		conex = conectar()
+		cursor = conex.cursor()
 
-        #rows = cursor.fetchall()  #lista de elementos que contiene la query
+		try:
+			cursor.execute(
+			"INSERT INTO alquilados VALUES (" + str(reservado.cod_id) +
+											",\'" + str(reservado.fechaInicio) + "\'" +
+											",\'" + str(reservado.diaSemana) + "\'" +
+											",\'" + str(reservado.fechaFin) + "\'" + 
+											",\'" + str(reservado.horaInicio) + "\'" +
+											",\'" + str(reservado.horaFin) + "\'" +
+											"," + str(reservado.id_grupo) + ");"
+			)
+		except Exception as e:
+			return e
 
-        num2 = cursor.execute(
-            'SELECT COUNT(*) \
-             FROM reservados ;')
-        
-        conex.close()
-    
-        if num2 == num:
-            return lanzarError("Problema reservando, intentelo de nuevo", 404, "Error", "about:blank")
+		conex.commit()
+		conex.close()
 
-        else:
-            return lanzarError("Espacio Reservado correctamente!!", 200, "Genial!", "about:blank")
-		
-	
-    #return 'do some magic!'
+		return "Reserva correcta"
+
+	else:
+		return "Peto json"
